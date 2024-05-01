bf89364e80702a8f3085f52be3c31016aecb86e3
danajackson2 <danajackson2@gmail.com>
2024-05-01 12:48:39 -0700

    day 7 part 1 minor cleanup

--- day_7/day_7_code.js
+++ day_7/day_7_code.js
@@ -1,25 +1,12 @@
-import { INPUT } from "./day_7_input.js";
-const CARD_VALUE = {
-  2: 1,
-  3: 2,
-  4: 3,
-  5: 4,
-  6: 5,
-  7: 6,
-  8: 7,
-  9: 8,
-  T: 9,
-  J: 10,
-  Q: 11,
-  K: 12,
-  A: 13,
-};
+import { INPUT, CARD_VALUE } from "./day_7_input.js";
 
-function handRank(hand) {
+function getHandRank(hand) {
   const handMap = {};
+
   hand.split("").forEach((card) => {
     handMap[card] ? (handMap[card] += 1) : (handMap[card] = 1);
   });
+
   const vals = Object.values(handMap);
 
   if (vals.includes(5)) {
@@ -43,23 +30,27 @@ function handRank(hand) {
   }
 }
 
-function compareSameHands(a, b) {
-  let idx = 0;
+function compareHands(a, b) {
+  const aRank = getHandRank(a);
+  const bRank = getHandRank(b);
 
-  while (a[idx] === b[idx]) {
-    idx++;
+  if (aRank === bRank) {
+    let idx = 0;
+
+    while (a[idx] === b[idx]) {
+      idx++;
+    }
+
+    return CARD_VALUE[a[idx]] - CARD_VALUE[b[idx]];
+  } else {
+    return aRank - bRank;
   }
-
-  return CARD_VALUE[a[idx]] - CARD_VALUE[b[idx]];
 }
 
 function getAnswer() {
   let result = 0;
 
-  INPUT.sort((a, b) => {
-    const rankDiff = handRank(a[0]) - handRank(b[0]);
-    return rankDiff === 0 ? compareSameHands(a[0], b[0]) : rankDiff;
-  });
+  INPUT.sort((a, b) => compareHands(a[0], b[0]));
 
   INPUT.forEach((item, idx) => {
     result += item[1] * (idx + 1);
